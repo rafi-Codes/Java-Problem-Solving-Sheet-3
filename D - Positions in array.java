7f4cd5bcd3a6d076a9202a9d934b55c7ac862460
Mohammad Rafiul Hasan <rafihasan27rafi@gmail.com>
2025-10-20 22:03:33 +0600

    Add codeforces class to read array positions

---
+++ D - Positions in array.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class codeforces {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+
+            if (arr[i] <= 10)
+              System.out.println("A" + "[" + i+ "]" + " = " + arr[i]); ;
+        }
+        sc.close();
+    }
+}
